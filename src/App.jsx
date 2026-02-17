9c09452a7d79bc4eb306bb513d9866f96d108276
tolulope-mo <olagokemarvellous@gmail.com>
2026-02-17 07:27:39 +0100

    created btns and organized file systemn and pushed to marvyBranch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import Button from './components/buttons/button'
 import './App.css'
+import { Routes, Route} from "react-router-dom";
+import LandingPage from './pages/landingPage/landingPage'
 
 function App() {
 
-  return (
-    <div>Hi Queen and Fatma</div>
-  )
+  return <>
+    <div>Hi Queen and Fatma, add your routes </div>
+     <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+  </>
 }
 
 export default App
